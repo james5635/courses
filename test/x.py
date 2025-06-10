7bce663c2cc4f7046cb2d91e00f94872425e4153
james5635 <brorojame2029@gmail.com>
2025-06-10 09:39:58 +0000

    finish week 04

---
+++ test/x.py
@@ -0,0 +1,8 @@
+from numpy.typing import ArrayLike
+def x( *args:  str | tuple | int | int | ArrayLike,
+    scalex: bool = True,
+    scaley: bool = True,
+    data=None,
+    **kwargs):
+    pass
+x('str')
