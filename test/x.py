c44c83c6fe58f080a52db20bc70fa9f9d06d84bc
james5635 <brorojame2029@gmail.com>
2025-07-12 17:22:35 +0700

    add Software Design and Architecture

--- test/x.py
+++ test/x.py
@@ -1,8 +1,30 @@
-from numpy.typing import ArrayLike
-def x( *args:  str | tuple | int | int | ArrayLike,
-    scalex: bool = True,
-    scaley: bool = True,
-    data=None,
-    **kwargs):
-    pass
-x('str')
+# from numpy.typing import ArrayLike
+# def x( *args:  str | tuple | int | int | ArrayLike,
+#     scalex: bool = True,
+#     scaley: bool = True,
+#     data=None,
+#     **kwargs):
+#     pass
+# x('str')
+
+
+def x():
+    1
+import numpy as n
+
+y: str = 20
+import math
+import json
+
+print("x is None " if x() == None else "x is not None")
+"""
+>>> print("hello")
+hello
+
+"""
+
+
+
+
+from doctest import testmod
+testmod(verbose=True)
